becf4468240144dc96b9a6f609733e376ab2553e
Padungkiat <aomsinkaky14@gmail.com>
2023-07-08 14:30:10 +0700

    edit design

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -57,7 +57,7 @@ class _LoginPageState extends State<LoginPage> {
                   child: Column(
                     children: [
                       Text(
-                        'SIGN IN',
+                        'Sign in',
                         style: TextStyle(
                             fontSize: 60, fontWeight: FontWeight.bold),
                       ),
