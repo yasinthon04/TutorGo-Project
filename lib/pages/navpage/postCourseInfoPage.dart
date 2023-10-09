e9af2803590b7c3f26c99485a018ea3f696c0e26
Padungkiat <aomsinkaky14@gmail.com>
2023-10-09 14:49:17 +0700

    Add enroll job button

--- lib/pages/navpage/postCourseInfoPage.dart
+++ lib/pages/navpage/postCourseInfoPage.dart
@@ -1,17 +1,158 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:tutorgo/pages/widget/header_widget.dart';
 import 'package:tutorgo/pages/widget/editPost.dart';
 
-class PostCourseInfoPage extends StatelessWidget {
+class PostCourseInfoPage extends StatefulWidget {
   final Map<String, dynamic> postCourseData;
   final String? postCourseId;
 
   const PostCourseInfoPage({required this.postCourseData, this.postCourseId});
 
+  @override
+  _PostCourseInfoPageState createState() => _PostCourseInfoPageState();
+}
+
+class _PostCourseInfoPageState extends State<PostCourseInfoPage> {
+  bool isEnrolled = false;
   bool get isStudent {
-    return postCourseData['userId'] == FirebaseAuth.instance.currentUser?.uid;
+    return widget.postCourseData['userId'] ==
+        FirebaseAuth.instance.currentUser?.uid;
+  }
+
+  void _enrollInCourse(BuildContext context, String courseId) async {
+    if (FirebaseAuth.instance.currentUser != null) {
+      final tutorId = FirebaseAuth.instance.currentUser!.uid;
+
+      try {
+        final tutorRef =
+            FirebaseFirestore.instance.collection('users').doc(tutorId);
+
+        await tutorRef.update({
+          'jobEnrolled': FieldValue.arrayUnion([widget.postCourseId]),
+        });
+
+        // Show success message or navigate to a different screen.
+      } catch (error) {
+        print('Error enrolling tutor: $error');
+      }
+    } else {
+      // Handle the case when the user is not authenticated.
+      // You might want to show a login prompt or navigate to the login screen.
+    }
+  }
+
+  void _requestEnrollInCourse(
+      BuildContext parentContext, String courseId) async {
+    if (FirebaseAuth.instance.currentUser != null) {
+      final tutorId = FirebaseAuth.instance.currentUser!.uid;
+
+      try {
+        final tutorRef =
+            FirebaseFirestore.instance.collection('users').doc(tutorId);
+
+        final courseRef = FirebaseFirestore.instance
+            .collection('postCourse')
+            .doc(widget.postCourseId);
+
+        // Add the tutor's ID to the list of requested tutors
+        await courseRef.update({
+          'requestedTutors': FieldValue.arrayUnion([tutorId]),
+        });
+
+        // Show the "Waiting for Confirmation" dialog
+        showDialog(
+          context: parentContext, // Use the provided parentContext here
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Waiting for Confirmation'),
+              content: Text('Your enrollment request is pending confirmation.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
+
+        // You can also consider refreshing the UI to reflect the pending status
+        // or showing a message on the UI.
+      } catch (error) {
+        print('Error requesting enrollment: $error');
+      }
+    } else {
+      // Handle the case when the user is not authenticated.
+      // You might want to show a login prompt or navigate to the login screen.
+    }
+  }
+
+  Future<bool> _showTutorEnrollConfirmation(BuildContext context) async {
+    return await showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Enroll as Tutor'),
+              content: Text(
+                'Do you want to enroll as a tutor for this job?',
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, false); // User canceled
+                  },
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context, true); // User confirmed
+                  },
+                  child: Text(
+                    'Enroll',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
+        ) ??
+        false; // Return false if dialog is dismissed
+  }
+
+  void _cancelEnrollment(BuildContext context, String courseId) async {
+    if (FirebaseAuth.instance.currentUser != null) {
+      final tutorId = FirebaseAuth.instance.currentUser!.uid;
+
+      try {
+        final tutorRef =
+            FirebaseFirestore.instance.collection('users').doc(tutorId);
+
+        await tutorRef.update({
+          'jobEnrolled': FieldValue.arrayRemove([widget.postCourseId]),
+        });
+
+        // Show success message or navigate to a different screen.
+        setState(() {
+          isEnrolled = false;
+        });
+      } catch (error) {
+        print('Error canceling enrollment: $error');
+      }
+    }
   }
 
   @override
@@ -44,14 +185,14 @@ class PostCourseInfoPage extends StatelessWidget {
             IconButton(
               onPressed: () {
                 // Navigate to the edit page
-                if (postCourseId != null) {
+                if (widget.postCourseId != null) {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
                       builder: (context) => EditPostPage(
-                        postCourseData: postCourseData,
+                        postCourseData: widget.postCourseData,
                         postCourseId:
-                            postCourseId, // Pass the postCourseId here
+                            widget.postCourseId, // Pass the postCourseId here
                       ),
                     ),
                   );
@@ -84,7 +225,7 @@ class PostCourseInfoPage extends StatelessWidget {
                     borderRadius: BorderRadius.circular(
                         10), // Adjust the radius as needed
                     child: Image.network(
-                      postCourseData['imageUrl'] ?? '',
+                      widget.postCourseData['imageUrl'] ?? '',
                       height: 250,
                       width: 350,
                       fit: BoxFit.cover,
@@ -92,10 +233,8 @@ class PostCourseInfoPage extends StatelessWidget {
                   ),
                   SizedBox(height: 5),
                   Text(
-                    'Course Name: ${postCourseData['courseName']}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20),
+                    'Course Name: ${widget.postCourseData['courseName']}',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
                   ),
                   SizedBox(height: 5),
                   RichText(
@@ -110,7 +249,7 @@ class PostCourseInfoPage extends StatelessWidget {
                           ),
                         ),
                         TextSpan(
-                          text: '${postCourseData['price']} ',
+                          text: '${widget.postCourseData['price']} ',
                           style: TextStyle(
                             color: Colors.black,
                             fontSize: 16,
@@ -131,7 +270,7 @@ class PostCourseInfoPage extends StatelessWidget {
                               fontSize: 16),
                         ),
                         TextSpan(
-                          text: '${postCourseData['address']}',
+                          text: '${widget.postCourseData['address']}',
                           style: TextStyle(
                               color: Colors.black,
                               fontSize:
@@ -141,13 +280,14 @@ class PostCourseInfoPage extends StatelessWidget {
                             WidgetSpan(
                               child: GestureDetector(
                                 onTap: () {
-                                  if (postCourseData['googleMapsLink'] !=
+                                  if (widget.postCourseData['googleMapsLink'] !=
                                           null &&
-                                      postCourseData['googleMapsLink']
+                                      widget.postCourseData['googleMapsLink']
                                           .isNotEmpty) {
                                     print(
-                                        'Google Maps Link: ${postCourseData['googleMapsLink']}');
-                                    launch(postCourseData['googleMapsLink']);
+                                        'Google Maps Link: ${widget.postCourseData['googleMapsLink']}');
+                                    launch(widget
+                                        .postCourseData['googleMapsLink']);
                                   }
                                 },
                                 child: Icon(
@@ -176,13 +316,56 @@ class PostCourseInfoPage extends StatelessWidget {
                           ),
                         ),
                         TextSpan(
-                          text: '${postCourseData['contactInfo']} ',
+                          text: '${widget.postCourseData['contactInfo']} ',
                           style: TextStyle(
                             color: Colors.black,
                             fontSize: 16,
                           ),
                         ),
                       ],
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () async {
+                      if (isEnrolled) {
+                        if (widget.postCourseId != null) {
+                          _cancelEnrollment(context, widget.postCourseId!);
+                        } else {
+                          print('Invalid postCourseId');
+                        }
+                      } else {
+                        bool shouldEnroll =
+                            await _showTutorEnrollConfirmation(context);
+
+                        if (shouldEnroll) {
+                          if (widget.postCourseId != null) {
+                            _enrollInCourse(context, widget.postCourseId!);
+                          } else {
+                            print('Invalid postCourseId');
+                          }
+                        }
+                      }
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 10),
+                      child: Text(
+                        isEnrolled ? 'Cancel Enrollment' : 'Enroll as Tutor',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      primary: isEnrolled
+                          ? Color.fromARGB(255, 221, 42, 29)
+                          : Theme.of(context).hintColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
+                      elevation: 4,
                     ),
                   ),
                 ],
